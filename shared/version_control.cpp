ef404a92de92fdb5bff2bb6874ba03b01d76aa1c
I.Kuruch <ikuruch@gmail.com>
2011-12-08 08:37:52 +0200

    off git ver control

--- shared/version_control.cpp
+++ shared/version_control.cpp
@@ -16,30 +16,30 @@ GitVC::GitVC()
     char version[128];
     char str_file[] = "../.git/logs/HEAD";
 
-    fp=fopen(str_file,"rb");
-    if (fp)
-    {
-        fseek(fp, 0, SEEK_END); ///go to end
-        len=ftell(fp) + 2; ///get position at end (length) + reserved 2 bytes
-        fseek(fp, 0, SEEK_SET); ///go to begin.
-        buf=(char *)malloc(len); ///malloc buffer
-        memset(buf, 0, len);
-        fread(buf, len, 1, fp); ///read into buffer
-        fclose(fp);
-	find_buf = strstr(buf, "commit:");
-	while(find_buf)
-	{
-	  last_ver_ptr = find_buf;
-	  find_buf = strstr(find_buf + 1, "commit:");
-	}
-	sscanf(last_ver_ptr - 17, "%s ", version);
-	_minor_version = version;
-	free(buf);
-    }
-    else
-    {
-        traceerr("GIT config file = %s, load error", str_file);
-    }
+//     fp=fopen(str_file,"rb");
+//     if (fp)
+//     {
+//         fseek(fp, 0, SEEK_END); ///go to end
+//         len=ftell(fp) + 2; ///get position at end (length) + reserved 2 bytes
+//         fseek(fp, 0, SEEK_SET); ///go to begin.
+//         buf=(char *)malloc(len); ///malloc buffer
+//         memset(buf, 0, len);
+//         fread(buf, len, 1, fp); ///read into buffer
+//         fclose(fp);
+// 	find_buf = strstr(buf, "commit:");
+// 	while(find_buf)
+// 	{
+// 	  last_ver_ptr = find_buf;
+// 	  find_buf = strstr(find_buf + 1, "commit:");
+// 	}
+// 	sscanf(last_ver_ptr - 17, "%s ", version);
+// 	_minor_version = version;
+// 	free(buf);
+//     }
+//     else
+//     {
+//         traceerr("GIT config file = %s, load error", str_file);
+//     }
     _build_version = intToString(time(NULL));
     _major_version = MAJOR_VERSION;
     _full_version = _major_version + _minor_version;
