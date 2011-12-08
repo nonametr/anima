a05f7f54a2d7679cb0e4cd4211e9caf84dc67b65
I.Kuruch <ikuruch@gmail.com>
2011-12-08 08:23:04 +0200

    make files add

---
+++ build/CMakeFiles/anima_game.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nonametr/projects/anima/game_server/game_server.cpp" "/home/nonametr/projects/anima/build/CMakeFiles/anima_game.dir/game_server/game_server.o"
+  "/home/nonametr/projects/anima/game_server/main_game.cpp" "/home/nonametr/projects/anima/build/CMakeFiles/anima_game.dir/game_server/main_game.o"
+  "/home/nonametr/projects/anima/game_server/shard.cpp" "/home/nonametr/projects/anima/build/CMakeFiles/anima_game.dir/game_server/shard.o"
+  "/home/nonametr/projects/anima/shared/common.cpp" "/home/nonametr/projects/anima/build/CMakeFiles/anima_game.dir/shared/common.o"
+  "/home/nonametr/projects/anima/shared/config.cpp" "/home/nonametr/projects/anima/build/CMakeFiles/anima_game.dir/shared/config.o"
+  "/home/nonametr/projects/anima/shared/json.cpp" "/home/nonametr/projects/anima/build/CMakeFiles/anima_game.dir/shared/json.o"
+  "/home/nonametr/projects/anima/shared/json_value.cpp" "/home/nonametr/projects/anima/build/CMakeFiles/anima_game.dir/shared/json_value.o"
+  "/home/nonametr/projects/anima/shared/net_core.cpp" "/home/nonametr/projects/anima/build/CMakeFiles/anima_game.dir/shared/net_core.o"
+  "/home/nonametr/projects/anima/shared/periodic_thread_caller.cpp" "/home/nonametr/projects/anima/build/CMakeFiles/anima_game.dir/shared/periodic_thread_caller.o"
+  "/home/nonametr/projects/anima/shared/socket.cpp" "/home/nonametr/projects/anima/build/CMakeFiles/anima_game.dir/shared/socket.o"
+  "/home/nonametr/projects/anima/shared/thread.cpp" "/home/nonametr/projects/anima/build/CMakeFiles/anima_game.dir/shared/thread.o"
+  "/home/nonametr/projects/anima/shared/thread_core.cpp" "/home/nonametr/projects/anima/build/CMakeFiles/anima_game.dir/shared/thread_core.o"
+  "/home/nonametr/projects/anima/shared/version_control.cpp" "/home/nonametr/projects/anima/build/CMakeFiles/anima_game.dir/shared/version_control.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ build/CMakeFiles/anima_game.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/anima_game.dir/game_server/main_game.o"
+  "CMakeFiles/anima_game.dir/game_server/game_server.o"
+  "CMakeFiles/anima_game.dir/game_server/shard.o"
+  "CMakeFiles/anima_game.dir/shared/common.o"
+  "CMakeFiles/anima_game.dir/shared/config.o"
+  "CMakeFiles/anima_game.dir/shared/socket.o"
+  "CMakeFiles/anima_game.dir/shared/net_core.o"
+  "CMakeFiles/anima_game.dir/shared/thread.o"
+  "CMakeFiles/anima_game.dir/shared/thread_core.o"
+  "CMakeFiles/anima_game.dir/shared/periodic_thread_caller.o"
+  "CMakeFiles/anima_game.dir/shared/json.o"
+  "CMakeFiles/anima_game.dir/shared/json_value.o"
+  "CMakeFiles/anima_game.dir/shared/version_control.o"
+  "anima_game.pdb"
+  "anima_game"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/anima_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
